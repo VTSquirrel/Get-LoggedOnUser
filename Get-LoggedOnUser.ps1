0fb7c8ceff304c20a0fcdfc34c455531daef1f6c
VTSquirrel <59540803+VTSquirrel@users.noreply.github.com>
2021-03-31 11:31:14 -0400

    Initial Commit

---
+++ Get-LoggedOnUser.ps1
@@ -0,0 +1,34 @@
+﻿Function Get-LoggedOnUser{
+     [CmdletBinding()]
+     param
+     (
+         [Parameter()]
+         [ValidateNotNullOrEmpty()]
+         [string]$ComputerName = $env:COMPUTERNAME,
+         [boolean] $OutputToHost = $false
+     )
+     $Result = New-Object System.Collections.Generic.List[System.Object]
+
+     foreach ($comp in $ComputerName){
+        if(Test-Connection $comp -Quiet -Count 1){
+            $Result.Add([PSCustomObject]@{
+                Computer = $comp
+                User = (Get-WmiObject -Class win32_computersystem -ComputerName $comp).UserName
+            })
+        }else{
+            $Result.Add([PSCustomObject]@{
+                Computer = $comp
+                User = "Unable to Connect"
+            })
+            if ($OutputToHost){
+                Write-Host "Unable to establish remote connection to $comp" -ForegroundColor Red
+            }
+        }
+     }
+     
+     if ($OutputToHost){
+        $Result | ft -a
+     }else{
+        return $Result
+     }
+ }
